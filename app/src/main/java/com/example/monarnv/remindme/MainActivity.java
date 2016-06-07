2aeeeff597767f543c666c9d5415e308623b0b3d
monarnv <nadya.rnv@gmail.com>
2016-06-07 23:01:54 +0300

    MainActivity was added

---
+++ app/src/main/java/com/example/monarnv/remindme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.monarnv.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
