2986ed75cc8ec5757c8a8e01f4e7bb77ae5980a3
monarnv <nadya.rnv@gmail.com>
2016-06-10 20:43:15 +0300

    Added toolbar and Search menu item

--- app/src/main/java/com/example/monarnv/remindme/MainActivity.java
+++ app/src/main/java/com/example/monarnv/remindme/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.monarnv.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends Activity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
