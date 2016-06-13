fc68481b3aabf7f4a051d50e881731556077e670
monarnv <nadya.rnv@gmail.com>
2016-06-13 17:17:50 +0300

    Added navigation view sceleton and custom theme.

--- app/src/main/java/com/example/monarnv/remindme/MainActivity.java
+++ app/src/main/java/com/example/monarnv/remindme/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.monarnv.remindme;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
     }
 
     private void initToolbar() {
@@ -22,4 +27,9 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle(R.string.app_name);
         toolbar.inflateMenu(R.menu.menu);
     }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
 }
